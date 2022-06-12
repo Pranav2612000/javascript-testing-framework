7a28c3c40c64e3b6630b302a39178e10a81f77ab
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-06-12 21:40:11 +0530

    improv: read code parallely using jest-worker and worker threads

--- index.mjs
+++ index.mjs
@@ -2,9 +2,13 @@ import JestHasteMap from "jest-haste-map";
 import {cpus} from "os";
 import {dirname} from "path";
 import {fileURLToPath} from "url";
-import fs from "fs";
+import {Worker} from "jest-worker";
+import {join} from "path";
 
 const root = dirname(fileURLToPath(import.meta.url));
+const worker = new Worker(join(root, 'worker.js'), {
+  enableWorkerThreads: true
+});
 
 const hasteMapOptions = {
   extensions: ["js"],
@@ -23,7 +27,9 @@ const testFiles = hasteFS.matchFilesWithGlob(["**/*.test.js"]);
 
 await Promise.all(
   Array.from(testFiles).map(async (testFile) => {
-    const code = await fs.promises.readFile(testFile, "utf8");
-    console.log(testFile + ":\n" + code);
+    const testResult = await worker.runTest(testFile);
+    console.log(testResult);
   })
 );
+
+worker.end();
