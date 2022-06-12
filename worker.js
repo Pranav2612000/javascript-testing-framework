1e16bda356c66e7d11d4c0d6a9765643400851c5
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-06-12 21:43:38 +0530

    feat: build system to allow evaluating tests

--- worker.js
+++ worker.js
@@ -2,5 +2,15 @@ const fs = require("fs");
 
 exports.runTest = async function(testFile) {
   const code = await fs.promises.readFile(testFile, "utf8");
-  return `worker id: ${process.env.JEST_WORKER_ID}\nfile: ${testFile}:\n code: ${code}`;
+  const testResult = {
+    success: false,
+    errorMessage: null
+  };
+  try {
+    eval(code);
+    testResult.success = true;
+  } catch (err) {
+    testResult.errorMessage = err.message;
+  }
+  return testResult;
 }
