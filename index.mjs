35406b56199119e80717520e7ec0ac77a84d29ba
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-06-12 21:25:47 +0530

    improv: use jest-haste-map for efficient searching through the files

--- index.mjs
+++ index.mjs
@@ -1,5 +1,23 @@
-import glob from "glob";
+import JestHasteMap from "jest-haste-map";
+import {cpus} from "os";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
 
-const testFiles = glob.sync("**/*.test.js");
+const root = dirname(fileURLToPath(import.meta.url));
+
+const hasteMapOptions = {
+  extensions: ["js"],
+  maxWorkers: cpus().length,
+  name: "building_a_test_framework",
+  platforms: [],
+  rootDir: root,
+  roots: [root]
+}
+
+const hasteMap = new JestHasteMap.default(hasteMapOptions);
+await hasteMap.setupCachePath(hasteMapOptions);
+
+const {hasteFS} = await hasteMap.build();
+const testFiles = hasteFS.matchFilesWithGlob(["**/*.test.js"]);
 
 console.log(testFiles);
