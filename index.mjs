a6107d794302c4dce1b3a4c42f86f7dc60cadaa5
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-06-12 21:50:46 +0530

    feat: beautify the ouput messages using chalk

--- index.mjs
+++ index.mjs
@@ -1,9 +1,10 @@
 import JestHasteMap from "jest-haste-map";
 import {cpus} from "os";
+import chalk from "chalk";
 import {dirname} from "path";
 import {fileURLToPath} from "url";
 import {Worker} from "jest-worker";
-import {join} from "path";
+import {join, relative} from "path";
 
 const root = dirname(fileURLToPath(import.meta.url));
 const worker = new Worker(join(root, 'worker.js'), {
@@ -27,8 +28,15 @@ const testFiles = hasteFS.matchFilesWithGlob(["**/*.test.js"]);
 
 await Promise.all(
   Array.from(testFiles).map(async (testFile) => {
-    const testResult = await worker.runTest(testFile);
-    console.log(testResult);
+    const {success, errorMessage} = await worker.runTest(testFile);
+    const status = success 
+      ? chalk.green.inverse.bold(' PASS ')
+      : chalk.red.inverse.bold(' FAIL ');
+
+    console.log(status + ' ' + chalk.dim(relative(root, testFile)));
+    if (!success) {
+      console.log(' ' + errorMessage);
+    }
   })
 );
 
